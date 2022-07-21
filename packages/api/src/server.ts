ded265219e0c87165620c6efe91cb69791f386d2
Rakesh Ravuri <rravuri@gmail.com>
2022-07-22 00:52:32 +0530

    basic routing controllers

---
+++ packages/api/src/server.ts
@@ -0,0 +1,10 @@
+import App from '@/app/app';
+import { IndexController } from '@controllers/index.controller';
+// import { AuthController } from '@controllers/auth.controller';
+// import { UsersController } from '@controllers/users.controller';
+import validateEnv from '@utils/validateEnv';
+
+validateEnv();
+
+const app = new App([IndexController]);
+app.listen();
